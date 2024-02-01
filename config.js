681f52fad79783596e60618ded8d48cbb4911425
Bokko99 <158235423+Bokko99@users.noreply.github.com>
2024-02-01 21:46:02 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,12 +7,12 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['13023606236', 'Boko', true],
+  ['13023606236', 'boko', true], 
   [''] 
 ] //Number of owners
 
-//global.pairingNumber = "" //put your bot number here
+//global.pairingNumber = "13023606236" //put your bot number here
 
 global.mods = ['917605902011','32460248586'] 
 global.prems = ['917605902011', '32460248586', '919398758484']
